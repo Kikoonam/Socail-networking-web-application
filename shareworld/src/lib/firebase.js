3a1c463a8e55bf32d7c1bf14ca6383ca6f4d1761
Hongduzi Saiershanbayi <92166985+Kikoonam@users.noreply.github.com>
2023-07-24 20:09:23 +0200

    Added some code

---
+++ shareworld/src/lib/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1eWmbMBZixQRV75RTFXOeKNUUcAQqNX4",
+  authDomain: "shareworld-2e1b9.firebaseapp.com",
+  projectId: "shareworld-2e1b9",
+  storageBucket: "shareworld-2e1b9.appspot.com",
+  messagingSenderId: "220337612514",
+  appId: "1:220337612514:web:961b75bba9ecfc5423e0bb",
+  measurementId: "G-VVEQZJV9D0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app)
+const analytics = getAnalytics(app);
+export { app, firestore };
