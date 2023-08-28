af62b4e5990ed64d8839a5f2e06b93b75a7351b2
Hongduzi Saiershanbayi <92166985+Kikoonam@users.noreply.github.com>
2023-08-28 20:44:57 +0200

    Fixed function doesusernameexits

--- shareworld/src/services/firebase.js
+++ shareworld/src/services/firebase.js
@@ -1,9 +1,10 @@
 import { firestore } from '../lib/firebase';
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs,getCountFromServer } from "firebase/firestore";
 
 export async function doesUsernameExist(username) {
-    const q = query(collection(firestore, "users"), where("username", "==", username));
-
-  return q>0;
+  const q = query(collection(firestore, "users"), where("username", "==", username));
+  const snapshot = await getCountFromServer(q);
+  
+  return snapshot.data().count>0;
 }
 
